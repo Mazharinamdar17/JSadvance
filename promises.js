9a9d0bf528ee7090fd1de896ddfaf8b5777515a2
Mazhar <129265463+Mazharinamdar17@users.noreply.github.com>
2025-01-31 11:24:03 -0800

    promisified code

--- promises.js
+++ promises.js
@@ -11,3 +11,15 @@ function callback() {
 }
 
 p.then(callback)
+//
+
+function setTimeoutPromisified(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  function callback() {
+      console.log("3 seconds have passed");
+  }
+  
+  setTimeoutPromisified(3000).then(callback)
+  
